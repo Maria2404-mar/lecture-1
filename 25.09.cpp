6f3c389d04bb83f8e83a8c8444e9bc6e9bb02df2
Maria2404-mar <mari.milchakovp.07@mail.ru>
2025-09-28 11:55:24 +0200

    Add files via upload

---
+++ 25.09.cpp
@@ -0,0 +1,26 @@
+﻿
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int x;
+    int y;
+    float w;
+    cin >> x >> y;
+    w = sqrt((x * x) + (y * y));
+    cout << w << endl;
+    std::cout << (x + y + w);
+    cout << "type of values:float" << endl;
+    cout << "type of values:int" << endl;
+    cout << "size in bits:" << sizeof(float) * 8 << endl;
+    cout << "size in bits:" << sizeof(int) * 8 << endl;
+    cout << "min value:" << numeric_limits<float>::lowest() << endl;
+    cout << "max value" << numeric_limits<float>::lowest() << endl;
+    cout << "min value:" << numeric_limits<int>::lowest() << endl;
+    cout << "max value" << numeric_limits<int>::lowest() << endl;
+
+    return 0;
+}
+
